744559fda16930f7c30e1648c26fc1cd993d5f2b
d3molite <42088225+d3molite@users.noreply.github.com>
2020-10-26 19:25:32 +0100

    more minor fixes

--- server.py
+++ server.py
@@ -63,4 +63,4 @@ def video_feed():
 if __name__ == '__main__':
 
     # defining server ip address and port
-	app.run(host='0.0.0.0',port='5000', debug=True, threaded=True)
+	app.run(host='0.0.0.0',port='5000', debug=False, threaded=True)
