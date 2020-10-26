064200a7e0e3e3ef47ca7bb53286582063dd8388
d3molite <42088225+d3molite@users.noreply.github.com>
2020-10-26 19:14:14 +0100

    a

--- scripts/camera.py
+++ scripts/camera.py
@@ -1,5 +1,4 @@
 # import the necessary packages
-import numpy as np
 import imutils
 import cv2
 
--- server.py
+++ server.py
@@ -1,12 +1,46 @@
 # main.py
 # import the necessary packages
 from flask import Flask, render_template, Response
-from scripts import camera
+import imutils
+import cv2
 
 app = Flask(__name__)
 
+class Camera:
+
+	def __init__(self, resolution, framerate, device):
+
+		self.w = resolution[0]
+		self.h = resolution[1]
+
+		# initialize the camera module
+		self.cap = cv2.VideoCapture(device)
+
+		# make video 1080p
+		self.cap.set(3, self.w)
+		self.cap.set(4, self.h)
+		self.cap.set(5, framerate)
+
+		self.stopped = False
+		self.grabbed = None
+		self.vFrame = None
+
+		pass
+
+	# function to push back a jpeg frame to the streaming server
+	def getFrame(self):
+
+		ret, frame = self.cap.read()
+		ret, self.vFrame = cv2.imencode('.jpg',frame)
+
+		return self.vFrame.tobytes()
+
+	def __del__(self):
+
+		self.cap.release()
+
 # initialize a new camera
-cam = camera.Camera(resolution=[1920,1080], framerate=60, device=0)
+cam = Camera(resolution=[1920,1080], framerate=60, device=0)
 
 
 @app.route('/')
